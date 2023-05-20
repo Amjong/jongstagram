041dc6d82b5a6fa7a0603c9bb8e2f1c8c64aa7f1
Jongbum <amjong2@gmail.com>
2023-05-20 15:33:55 +0900

    components/Header.tsx: Added icons to Header

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import { BsPlusSquare, BsSearch } from 'react-icons/bs';
+import { AiOutlineHome } from 'react-icons/ai';
 
 export default function Header() {
   return (
-    <div className='flex '>
+    <div className='flex'>
       <h1>Jongstagram</h1>
+      <AiOutlineHome></AiOutlineHome>
+      <BsPlusSquare></BsPlusSquare>
+      <BsSearch></BsSearch>
     </div>
   );
 }
