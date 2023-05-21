715822b9871061363089a6ff1d2ba18c45da15d7
Jongbum <amjong2@gmail.com>
2023-05-21 17:58:43 +0900

    page.tsx: Added Sign in button to main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
 import Image from 'next/image';
 
 export default function Home() {
-  return <main className=''></main>;
+  return (
+    <main className='flex flex-col items-center'>
+      <button className='rounded-lg border-2 border-pink-300 text-2xl mt-20 p-1'>
+        Sign in with Google
+      </button>
+    </main>
+  );
 }
