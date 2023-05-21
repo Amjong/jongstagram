5ee3d35209b07c210f31d64847612e1e144429df
Jongbum <amjong2@gmail.com>
2023-05-21 17:44:57 +0900

    components/Header.tsx: Styling Header component

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -4,11 +4,14 @@ import { AiOutlineHome } from 'react-icons/ai';
 
 export default function Header() {
   return (
-    <div className='flex'>
-      <h1>Jongstagram</h1>
-      <AiOutlineHome></AiOutlineHome>
-      <BsPlusSquare></BsPlusSquare>
-      <BsSearch></BsSearch>
+    <div className='flex justify-between m-1 border-b-2 border-gray-100'>
+      <h1 className='text-3xl font-bold mt-2 ml-1'>Jongstagram</h1>
+      <div className='flex space-x-4 text-2xl m-3 items-center'>
+        <AiOutlineHome></AiOutlineHome>
+        <BsSearch></BsSearch>
+        <BsPlusSquare></BsPlusSquare>
+        <button className='rounded-lg border-2 border-pink-300'>Sign in</button>
+      </div>
     </div>
   );
 }
