256d3563055de61158c4a0c9ad5eaf6b1611890e
Jongbum <amjong2@gmail.com>
2023-06-17 22:31:41 +0900

    Header.tsx: Extract nav icons to component

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,30 +1,20 @@
 import React from 'react';
-import { BsPlusSquare, BsSearch } from 'react-icons/bs';
-import { AiOutlineHome } from 'react-icons/ai';
 import LoginButton from './LoginButton';
-import { getUserSession } from '../service/session';
 import Profile from './Profile';
 import Link from 'next/link';
+import { getUserSession } from '../service/session';
+import NavIcons from './NavIcons';
 
 export default async function Header() {
   const session = await getUserSession();
   const text = session ? 'Sign out' : 'Sign in';
   return (
-    <div className='flex justify-between m-1 border-b-2 border-gray-100'>
+    <div className='sticky flex justify-between m-1 border-b-2 border-gray-100'>
       <h1 className='text-3xl font-bold mt-2 ml-1'>
         <Link href='/'>Jongstagram</Link>
       </h1>
       <div className='flex space-x-4 text-2xl m-3 items-center'>
-        <Link href='/'>
-          <AiOutlineHome></AiOutlineHome>
-        </Link>
-        <Link href='/search'>
-          <BsSearch></BsSearch>
-        </Link>
-        <Link href='/new'>
-          <BsPlusSquare></BsPlusSquare>
-        </Link>
-
+        <NavIcons></NavIcons>
         {session && (
           <Profile
             imageSource={session?.user?.image as string}
